8542fc61c2e8f2a65f48fcfa3d232a776931dd9c
Connor Harrison <connorh315@gmail.com>
2025-10-26 22:11:21 +0000

    Update version number

--- src/globals.h
+++ src/globals.h
@@ -4,6 +4,6 @@
 extern uint64_t g_base_addr;
 extern int g_mods_loaded;
 
-#define ANOMALY_VERSION "Anomaly BETA v1.0.1"
+#define ANOMALY_VERSION "Anomaly BETA v1.1.0"
 
 #define ANOMALY_INITIALISED ANOMALY_VERSION " Initialised!"
